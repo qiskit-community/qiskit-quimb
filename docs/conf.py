fcb699a24f17fa974e7012aadf18adc908f0908c
Jim Garrison <garrison@ibm.com>
2025-01-21 10:18:26 -0500

    Use `sphinx.ext.githubpages` to add `.nojekyll` file
    See https://www.sphinx-doc.org/en/master/usage/extensions/githubpages.html
    
    Closes #11

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.viewcode",
     "sphinx.ext.extlinks",
+    "sphinx.ext.githubpages",
     "sphinx_autodoc_typehints",
     "myst_parser",
     "nbsphinx",
